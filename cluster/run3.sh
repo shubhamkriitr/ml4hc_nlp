7c590e5adf3484d313175130612bb4b2dc930ed4
Shubham Kumar <kumarsh@student.ethz.ch>
2022-04-25 01:17:24 +0200

    update configs and add run scripts

---
+++ cluster/run3.sh
@@ -0,0 +1,5 @@
+# Activate Conda
+eval "$(/cluster/home/kumarsh/miniconda3/bin/conda shell.bash hook)"
+conda activate ml4hc_proj2 
+cd /cluster/scratch/kumarsh/ml4hc_nlp
+python src/trainingutil.py --config src/experiment_configs/exp_03_task2_ann_unfrozen_embeddings.yaml
